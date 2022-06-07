2605de0a061022bc4a6f03f2566604439f5e049d
Keks <keks@htmlacademy.ru>
2022-06-07 12:04:18 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,42 +1,3 @@
-// Функции для вызова рандомного целого числа
-function getRandomInt (a, b) {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-}
-
-// Функция для вызова рандомного числа с плавающей точкой
-function getRandomFloat (a, b, digits = 1) {
-  const lower = Math.min(Math.abs(a), Math.abs(b));
-  const upper = Math.max(Math.abs(a), Math.abs(b));
-
-  const result = Math.random() * (upper - lower) + lower;
-  return +result.toFixed(digits);
-}
-
-// Функция для вызова рандомного элемента из массива
-function getRandomElementFromArray(element) {
-  return element[getRandomInt(0, element.length - 1)];
-}
-
-// Функция для вызова рандомного количества элементов из массива
-
-function getRandomElementsFromArray (elements) {
-  const elementsLength = getRandomInt(1, elements.length);
-  const arrayRandomElements = [];
-
-  while (arrayRandomElements.length <= elementsLength) {
-    const randomElements = elements[getRandomInt(0, elements.length - 1)];
-
-    if (!arrayRandomElements.includes(randomElements)) {
-      arrayRandomElements.push(randomElements);
-    }
-  }
-  return arrayRandomElements;
-}
-
 // КОНСТАНТЫ
 const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const TIMES = ['12:00', '13:00', '14:00'];
@@ -52,13 +13,70 @@ const Coords = {
   LNG_MIN: 139.70000,
   LNG_MAX: 139.80000
 };
+const COUNT_ADS = 10;
+
+/**
+ * Функции для вызова рандомного целого числа
+ * @param {number} a — первое число
+ * @param {number} b — второе число
+ * @return {integer} — рандомное целое число
+ */
+function getRandomInt (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+/**
+ * Функция для вызова рандомного числа с плавающей точкой
+ * @param {number} a — первое число
+ * @param {number} b — второе число
+ * @param {number} digits — количество чисел после плавающей точки
+ * @return {float} — рандомное число с плавающей точкой
+ */
+function getRandomFloat (a, b, digits = 1) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+
+  const result = Math.random() * (upper - lower) + lower;
+  return +result.toFixed(digits);
+}
+
+/**
+ * Функция для вызова рандомного элемента из массива
+ * @param {Array} element — название массива, из которого будем выбирать рандомные элементы
+ * @return {string} — рандомный элемент массива
+ */
+function getRandomElementFromArray(element) {
+  return element[getRandomInt(0, element.length - 1)];
+}
+
+/**
+ * Функция для вызова рандомного количества элементов из массива
+ * @param {Array} elements — название массива, из которого будем выбирать рандомные элементы
+ * @return {Array} — новый массив с рандомными элементами основного массива
+ */
+function getRandomElementsFromArray (elements) {
+  const elementsLength = getRandomInt(1, elements.length);
+  const arrayRandomElements = [];
+
+  while (arrayRandomElements.length < elementsLength) {
+    const randomElements = getRandomElementFromArray(elements);
+
+    if (!arrayRandomElements.includes(randomElements)) {
+      arrayRandomElements.push(randomElements);
+    }
+  }
+  return arrayRandomElements;
+}
 
 /**
  * Функция для генерации объявления
  * @param {integer} index — индекс изображения, который будем передавать при генерации списка объявлений
  * @return {Object} — готовый объект объявления
  */
-
 const createAd = (index) => {
   const location = {
     lat: getRandomFloat(Coords.LAT_MIN, Coords.LAT_MAX),
@@ -70,7 +88,7 @@ const createAd = (index) => {
     },
     offer: {
       title: 'Лучшее место для путешествий по России',
-      address: 'LOCATION.lat, LOCATION.lng',
+      address: `${location.lat}, ${location.lng}`,
       price: getRandomInt(500, 1000),
       type: getRandomElementFromArray(TYPES),
       rooms: getRandomInt(1, 5),
@@ -90,5 +108,5 @@ const createAd = (index) => {
  * @param {integer} count — количество объявлений, которое хотим сгенерировать
  * @return {Array} — массив сгенерированных объявлений
  */
-
-const createAds = (count) => Array.from({length:count}, (x, i) => createAd(i));
+const createAds = (count) => Array.from({length:count}, (_, i) => createAd(i + 1));
+console.log(createAds(COUNT_ADS));
