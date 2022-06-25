d7b1d9924dcff981411fc114de9ddfa157601605
Alyona <alenka.inform@gmail.com>
2022-06-25 09:51:31 +0300

    Fix the mistakes of task #7

--- js/util.js
+++ js/util.js
@@ -1,9 +1,4 @@
-/**
- * Функции для вызова рандомного целого числа
- * @param {number} a — первое число
- * @param {number} b — второе число
- * @return {integer} — рандомное целое число
- */
+// Функции для вызова рандомного целого числа
 function getRandomInt (a, b) {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
@@ -12,13 +7,7 @@ function getRandomInt (a, b) {
   return Math.floor(result);
 }
 
-/**
- * Функция для вызова рандомного числа с плавающей точкой
- * @param {number} a — первое число
- * @param {number} b — второе число
- * @param {number} digits — количество чисел после плавающей точки
- * @return {float} — рандомное число с плавающей точкой
- */
+// Функция для вызова рандомного числа с плавающей точкой
 function getRandomFloat (a, b, digits = 1) {
   const lower = Math.min(Math.abs(a), Math.abs(b));
   const upper = Math.max(Math.abs(a), Math.abs(b));
@@ -27,20 +16,13 @@ function getRandomFloat (a, b, digits = 1) {
   return +result.toFixed(digits);
 }
 
-/**
- * Функция для вызова рандомного элемента из массива
- * @param {Array} element — название массива, из которого будем выбирать рандомные элементы
- * @return {string} — рандомный элемент массива
- */
+// Функция для вызова рандомного элемента из массива
+
 function getRandomElementFromArray(element) {
   return element[getRandomInt(0, element.length - 1)];
 }
 
-/**
- * Функция для вызова рандомного количества элементов из массива
- * @param {Array} elements — название массива, из которого будем выбирать рандомные элементы
- * @return {Array} — новый массив с рандомными элементами основного массива
- */
+// Функция для вызова рандомного количества элементов из массива
 function getRandomElementsFromArray (elements) {
   const elementsLength = getRandomInt(1, elements.length);
   const arrayRandomElements = [];
@@ -55,4 +37,13 @@ function getRandomElementsFromArray (elements) {
   return arrayRandomElements;
 }
 
-export {getRandomInt, getRandomFloat, getRandomElementFromArray, getRandomElementsFromArray};
+// Функция для удаления елемента
+function deleteElement (data, element) {
+  if (data) {
+    return (element.textContent = data);
+  } else {
+    return element.remove();
+  }
+}
+
+export {getRandomInt, getRandomFloat, getRandomElementFromArray, getRandomElementsFromArray, deleteElement};
