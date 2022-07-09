ea2af651910dc6e8e0db9b5645472efc1b733d3a
Keks <keks@htmlacademy.ru>
2022-07-09 11:07:32 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/filters.js
@@ -0,0 +1,11 @@
+// import { createMarker, clearMarkers} from './map.js';
+
+// const DEFAULT = 'any';
+
+// const filtersContainer = document.querySelector('.map__filters');
+// const filterType = filtersContainer.querySelector('#housing-type');
+// const filterPrice = filtersContainer.querySelector('#housing-price');
+// const filterRooms = filtersContainer.querySelector('#housing-rooms');
+// const filterGuests = filtersContainer.querySelector('#housing-guests');
+// const filterFeatureList = filtersContainer.querySelectorAll('[name="features"]');
+
